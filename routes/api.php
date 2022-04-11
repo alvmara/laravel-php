10748eab146c3e91c4bfb037f9b1fddd6e15c05d
alvmara <alvaromarzo104@gmail.com>
2022-04-11 19:43:32 +0200

    feat:añadido importaciones en api que faltaban

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GamesController;
+use App\Http\Controllers\PartyController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\MemberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
